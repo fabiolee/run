6e5c6745c09bc7b88b508c45482ff19c8b6b8be3
fabiolee <fabiolee59@gmail.com>
2018-09-12 11:26:52 +0800

    - Enhance search bar.

--- lib/home_tab.dart
+++ lib/home_tab.dart
@@ -25,11 +25,12 @@ class HomeTabSearchDelegate extends SearchDelegate<dom.Element> {
   Widget buildSuggestions(BuildContext context) {
     final Iterable<dom.Element> suggestions = query.isEmpty
         ? history
-        : data.where((dom.Element item) => item.text.contains(query));
+        : data.where((dom.Element item) =>
+            item.text.toLowerCase().contains(query.toLowerCase()));
 
     return new _SuggestionList(
       query: query,
-      suggestions: suggestions.map((dom.Element item) => item).toList(),
+      suggestions: suggestions.toList(),
       onSelected: (dom.Element suggestion) {
         query = suggestion.text;
         // TODO: Show results with API call.
@@ -41,7 +42,10 @@ class HomeTabSearchDelegate extends SearchDelegate<dom.Element> {
 
   @override
   Widget buildResults(BuildContext context) {
-    final List<dom.Element> searched = _getSearchList(data, query);
+    final List<dom.Element> searched = data
+        .where((dom.Element item) =>
+            item.text.toLowerCase().contains(query.toLowerCase()))
+        .toList();
     if (searched == null || searched.isEmpty) {
       return new Center(
         child: new Text(
@@ -98,17 +102,6 @@ class HomeTabSearchDelegate extends SearchDelegate<dom.Element> {
             )
     ];
   }
-
-  List<dom.Element> _getSearchList(List<dom.Element> list, String query) {
-    List<dom.Element> searchList = new List();
-    for (dom.Element item in list) {
-      String title = item.text;
-      if (title.contains(query)) {
-        searchList.add(item);
-      }
-    }
-    return searchList;
-  }
 }
 
 class _ResultList extends StatelessWidget {
@@ -123,77 +116,62 @@ class _ResultList extends StatelessWidget {
     return new ListView.builder(
       itemCount: results.length,
       itemBuilder: (BuildContext context, int i) {
-        final String result = results[i].text;
-        final int startIndex = result.indexOf(query);
-        final int endIndex = startIndex + query.length;
-        return new ListTile(
-          leading: query.isEmpty ? const Icon(Icons.history) : const Icon(null),
-          title: _buildRichText(context, result, startIndex, endIndex),
-          onTap: () {
-            searchDelegate.close(context, results[i]);
-          },
-        );
+        return _buildListViewRow(context, results[i]);
       },
     );
-    /*
-    final ThemeData theme = Theme.of(context);
+  }
+
+  Widget _buildListViewRow(BuildContext context, dom.Element item) {
+    final String text = item.text;
+    final int startIndex = text.toLowerCase().indexOf(query.toLowerCase());
+    final int endIndex = startIndex + query.length;
     return new GestureDetector(
+      child: new Container(
+          child: new Padding(
+              padding: new EdgeInsets.all(16.0),
+              child: new Row(
+                children: <Widget>[
+                  new Padding(
+                      padding: new EdgeInsets.only(right: 32.0),
+                      child: new Icon(Icons.history, color: Colors.black38)),
+                  new Expanded(
+                      child: new Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      _buildRichText(context, text, startIndex, endIndex),
+                    ],
+                  )),
+                ],
+              )),
+          decoration: new BoxDecoration(
+              border:
+                  new Border(bottom: new BorderSide(color: Colors.grey[200])))),
       onTap: () {
         searchDelegate.close(context, item);
       },
-      child: new Card(
-        child: new Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: new Column(
-            children: <Widget>[
-              new Text(title),
-              new Text(
-                item.text,
-                style: theme.textTheme.headline.copyWith(fontSize: 72.0),
-              ),
-            ],
-          ),
-        ),
-      ),
     );
-    */
   }
 
   Widget _buildRichText(
       BuildContext context, String text, int startIndex, int endIndex) {
     final ThemeData theme = Theme.of(context);
-    if (startIndex == 0) {
-      return new RichText(
-        text: new TextSpan(
-          text: text.substring(startIndex, endIndex),
-          style: theme.textTheme.subhead.copyWith(fontWeight: FontWeight.bold),
-          children: <TextSpan>[
-            new TextSpan(
-              text: text.substring(endIndex),
-              style: theme.textTheme.subhead,
-            ),
-          ],
-        ),
-      );
-    } else {
-      return new RichText(
-        text: new TextSpan(
-          text: text.substring(0, startIndex),
-          style: theme.textTheme.subhead,
-          children: <TextSpan>[
-            new TextSpan(
-              text: text.substring(startIndex, endIndex),
-              style:
-                  theme.textTheme.subhead.copyWith(fontWeight: FontWeight.bold),
-            ),
-            new TextSpan(
-              text: text.substring(endIndex),
-              style: theme.textTheme.subhead,
-            ),
-          ],
-        ),
-      );
-    }
+    return new RichText(
+      text: new TextSpan(
+        text: text.substring(0, startIndex),
+        style: theme.textTheme.subhead,
+        children: <TextSpan>[
+          new TextSpan(
+            text: text.substring(startIndex, endIndex),
+            style:
+                theme.textTheme.subhead.copyWith(fontWeight: FontWeight.bold),
+          ),
+          new TextSpan(
+            text: text.substring(endIndex),
+            style: theme.textTheme.subhead,
+          ),
+        ],
+      ),
+    );
   }
 }
 
@@ -209,18 +187,40 @@ class _SuggestionList extends StatelessWidget {
     return new ListView.builder(
       itemCount: suggestions.length,
       itemBuilder: (BuildContext context, int i) {
-        final String suggestion = suggestions[i].text;
-        final int startIndex = suggestion.indexOf(query);
-        final int endIndex = startIndex + query.length;
-        debugPrint(
-            'suggestion: $suggestion, query: $query, startIndex: $startIndex, endIndex: $endIndex');
-        return new ListTile(
-          leading: query.isEmpty ? const Icon(Icons.history) : const Icon(null),
-          title: _buildRichText(context, suggestion, startIndex, endIndex),
-          onTap: () {
-            onSelected(suggestions[i]);
-          },
-        );
+        return _buildListViewRow(context, suggestions[i]);
+      },
+    );
+  }
+
+  Widget _buildListViewRow(BuildContext context, dom.Element item) {
+    final String text = item.text;
+    final int startIndex = text.toLowerCase().indexOf(query.toLowerCase());
+    final int endIndex = startIndex + query.length;
+    debugPrint(
+        'text: $text, query: $query, startIndex: $startIndex, endIndex: $endIndex');
+    return new GestureDetector(
+      child: new Container(
+          child: new Padding(
+              padding: new EdgeInsets.all(16.0),
+              child: new Row(
+                children: <Widget>[
+                  new Padding(
+                      padding: new EdgeInsets.only(right: 32.0),
+                      child: new Icon(Icons.history, color: Colors.black38)),
+                  new Expanded(
+                      child: new Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      _buildRichText(context, text, startIndex, endIndex),
+                    ],
+                  )),
+                ],
+              )),
+          decoration: new BoxDecoration(
+              border:
+                  new Border(bottom: new BorderSide(color: Colors.grey[200])))),
+      onTap: () {
+        onSelected(item);
       },
     );
   }
@@ -228,37 +228,22 @@ class _SuggestionList extends StatelessWidget {
   Widget _buildRichText(
       BuildContext context, String suggestion, int startIndex, int endIndex) {
     final ThemeData theme = Theme.of(context);
-    if (startIndex == 0) {
-      return new RichText(
-        text: new TextSpan(
-          text: suggestion.substring(startIndex, endIndex),
-          style: theme.textTheme.subhead.copyWith(fontWeight: FontWeight.bold),
-          children: <TextSpan>[
-            new TextSpan(
-              text: suggestion.substring(endIndex),
-              style: theme.textTheme.subhead,
-            ),
-          ],
-        ),
-      );
-    } else {
-      return new RichText(
-        text: new TextSpan(
-          text: suggestion.substring(0, startIndex),
-          style: theme.textTheme.subhead,
-          children: <TextSpan>[
-            new TextSpan(
-              text: suggestion.substring(startIndex, endIndex),
-              style:
-                  theme.textTheme.subhead.copyWith(fontWeight: FontWeight.bold),
-            ),
-            new TextSpan(
-              text: suggestion.substring(endIndex),
-              style: theme.textTheme.subhead,
-            ),
-          ],
-        ),
-      );
-    }
+    return new RichText(
+      text: new TextSpan(
+        text: suggestion.substring(0, startIndex),
+        style: theme.textTheme.subhead,
+        children: <TextSpan>[
+          new TextSpan(
+            text: suggestion.substring(startIndex, endIndex),
+            style:
+                theme.textTheme.subhead.copyWith(fontWeight: FontWeight.bold),
+          ),
+          new TextSpan(
+            text: suggestion.substring(endIndex),
+            style: theme.textTheme.subhead,
+          ),
+        ],
+      ),
+    );
   }
 }
